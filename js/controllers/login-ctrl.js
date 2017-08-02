b897a70ba19122d0707b930df2910ad66f1c7e1c
Chris <chchow@umich.edu>
2017-08-02 15:00:32 -0400

    second

--- js/controllers/login-ctrl.js
+++ js/controllers/login-ctrl.js
@@ -2,5 +2,7 @@
 var app = angular.module("myModule");
 
 app.controller("loginController", function($scope, loginService) {
-
+  $scope.doStuff = function() {
+    loginService.checkLogin($scope.userInfo);
+  };
 });
--- js/controllers/profile-ctrl.js
+++ js/controllers/profile-ctrl.js
@@ -2,5 +2,6 @@
 var app = angular.module("myModule");
 
 app.controller("profileController", function($scope, loginService) {
-
+  $scope.userDetails = loginService.getProfile();
+  
 });
